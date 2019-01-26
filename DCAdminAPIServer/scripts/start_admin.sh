7538a92578be97be1ae7389fd44c003797a880b8
shseo@digicaps.com <shseo@digicaps.com>
2019-01-26 15:38:23 +0900

    [UPDATE]구동 스크립트에 pinpoint 설정 추가

--- DCAdminAPIServer/scripts/start_admin.sh
+++ DCAdminAPIServer/scripts/start_admin.sh
@@ -1,10 +1,10 @@
-# No Redirection
-#JAVA_HOME=/root/DcBlock/install/java-se-8u40-ri
-#nohup $JAVA_HOME/bin/java -jar /root/DcBlock/install/CaffeAPIServer-0.0.1.jar --spring.config.location=file:/root/DcBlock/application.yml 1>/dev/null 2>&1 &
-
-# stdout, stderr
 JAVA_HOME=/usr/bin
 APP_PATH=/opt/dcadmin/DCAdminAPI-0.0.1.jar
 CONFIG_PATH=/opt/dcadmin/application.properties
 
-nohup $JAVA_HOME/java -jar $APP_PATH --spring.config.location=$CONFIG_PATH 1>/var/log/dcadmin/stdout.log 2>/var/log/dcadmin/stderr.log &
+# pinpoint
+PINPOINT=/opt/pinpoint-agent/pinpoint-bootstrap-1.8.1.jar
+AGENT_ID=2
+APPLICATION_NAME=DCAdminAPI
+
+nohup $JAVA_HOME/java -javaagent:$PINPOINT -Dpinpoint.agentId=$AGENT_ID -Dpinpoint.applicationName=$APPLICATION_NAME -jar $APP_PATH --spring.config.location=$CONFIG_PATH 1>/var/log/dcadmin/stdout.log 2>/var/log/dcadmin/stderr.log &
